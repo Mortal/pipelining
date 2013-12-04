1c5c9fb610fa0db05db7922c11bc9b21d0244586
Mathias Rav <rav@cs.au.dk>
2013-12-04 11:15:50 +0100

    Compile with common.h/cpp

--- tpie/transform.cpp
+++ tpie/transform.cpp
@@ -1,4 +1,6 @@
 // -*- mode: c++; tab-width: 4; indent-tabs-mode: t; eval: (progn (c-set-style "stroustrup") (c-set-offset 'innamespace 0)); -*-
 // vi:set ts=4 sts=4 sw=4 noet :
 
+#include "../common/common.h"
+
 int main() { return 0; }
