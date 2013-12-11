d9a0a720facd7ed7115d28470cbd31bf40b4d5c8
Mathias Rav <rav@cs.au.dk>
2013-12-11 12:48:28 +0100

    Add modeline to stxxl/compare.h.

--- stxxl/compare.h
+++ stxxl/compare.h
@@ -1,3 +1,6 @@
+// -*- mode: c++; tab-width: 4; indent-tabs-mode: t; eval: (progn (c-set-style "stroustrup") (c-set-offset 'innamespace 0)); -*-
+// vi:set ts=4 sts=4 sw=4 noet :
+
 #ifndef COMPARE_H
 #define COMPARE_H
 
