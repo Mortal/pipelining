fa39218b343406f82fb3617ee6df959d200debcb
Mathias Rav <rav@cs.au.dk>
2013-12-11 12:08:05 +0100

    Remove constructors from point types, turning them into POD.

--- common/common.h
+++ common/common.h
@@ -28,31 +28,16 @@ struct program_options {
 struct point2 {
 	int x;
 	int y;
-
-	point2() = default;
-	point2(const point2 &) = default;
-	point2(point2 &&) = default;
-	point2(int x, int y) : x(x), y(y) {}
 };
 
 struct value_point {
 	point2 point;
 	float value;
-
-	value_point() = default;
-	value_point(const value_point &) = default;
-	value_point(value_point &&) = default;
-	value_point(point2 point, float value): point(point), value(value) {}
 };
 
 struct map_point {
 	point2 from;
 	point2 to;
-
-	map_point() = default;
-	map_point(const map_point &) = default;
-	map_point(map_point &&) = default;
-	map_point(point2 from, point2 to): from(from), to(to) {}
 };
 
 #endif // COMMON_COMMON_H
